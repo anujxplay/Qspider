0797d77bb81b23f6c9ed17e7932633c7879df9bb
anujxplay <anuj.knipss012@gmail.com>
2025-10-16 01:05:07 +0530

    update code

--- swapnum/while.py
+++ swapnum/while.py
@@ -6,7 +6,26 @@
 
 
     
-i = 100
-while i < 150:
-    print(i)
-    i += 1
+# i = 100
+# while i < 150:
+#     print(i)
+#     i += 1
+
+
+
+#Multiplication table
+n = int(input("Enter the number --"))
+for i in range(11):
+    print(n,"x",i,"=",n*i)
+
+
+
+
+
+
+
+#WAP to create a list containing n natural odd numbers
+# n = int(input("Enter a number: "))
+# list1 = {i for i in range(1,n+1) if i%2!=0}
+# print(list1)
+
