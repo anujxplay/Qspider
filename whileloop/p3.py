ce07e1b0c7df5315914e9d4e546f1cb7ee650d4f
anujxplay <anuj.knipss012@gmail.com>
2026-03-01 22:39:40 +0530

    update code

--- whileloop/p3.py
+++ whileloop/p3.py
@@ -1,14 +1,25 @@
+'''
 #1. WAP to print 1 to 100 natural numbers
 i=1
 while i<=50:
     print(i)
     i+=1
-    
-    
-    
+
+
+
+
 #2. WAP to print n natural numbers
 n=int(input("Enter a number --"))  
 i=1
 while i<=n:
     print(i)
-    i+=1      
+    i+=1    
+'''
+
+
+# WAP to print user name 10 times
+user = eval(input("Enter your number--"))
+i=1
+while i<=10:
+    print(user)
+    i+=1    
